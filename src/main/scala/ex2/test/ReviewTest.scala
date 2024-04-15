fa809a6558f3d0add60571a1155787ad8d08b718
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-15 11:17:25 +0200

    feat: add test for loadReviewWithParameters

--- src/main/scala/ex2/test/ReviewTest.scala
+++ src/main/scala/ex2/test/ReviewTest.scala
@@ -5,10 +5,17 @@ import ex2.Question
 import org.junit.Test
 
 class ReviewTest:
-    val confReview = ConferenceReviewing()
+    private val confReview = ConferenceReviewing()
 
     @Test def testLoadReview(): Unit = 
         val article = 0
         val scores = Map(Question.RELEVANCE -> 1)
         confReview.loadReview(article = article, scores = scores) 
-    
+    @Test def testLoadReviewWithParameters(): Unit =
+        val article = 0
+        val relevance = 1
+        val significance = 1
+        val confidence = 1
+        val fin = 1
+        confReview.loadReview(article, relevance, significance, confidence, fin)
+        
