44bbe3b56ea696e2651d38f59214c22454c8effe
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-15 11:09:12 +0200

    feat: add testLoadReview

--- src/main/scala/ex2/test/ReviewTest.scala
+++ src/main/scala/ex2/test/ReviewTest.scala
@@ -1,3 +1,14 @@
 package ex2.test
 
-class ReviewTest
+import ex2.ConferenceReviewing
+import ex2.Question
+import org.junit.Test
+
+class ReviewTest:
+    val confReview = ConferenceReviewing()
+
+    @Test def testLoadReview(): Unit = 
+        val article = 0
+        val scores = Map(Question.RELEVANCE -> 1)
+        confReview.loadReview(article = article, scores = scores) 
+    
