b7cdd2eaddb96ee17e1e8e76eb99b8ed50ddd3d0
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-15 18:41:17 +0200

    feat: init Indexed Sequences

--- src/main/scala/ex3/Collections.scala
+++ src/main/scala/ex3/Collections.scala
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
 import scala.collection.mutable.ListBuffer
 import java.util.Random
+import scala.collection.mutable.ArrayBuffer
 
 object PerformanceUtils:
   case class MeasurementResults[T](result: T, duration: FiniteDuration) extends Ordered[MeasurementResults[_]]:
@@ -30,18 +31,27 @@ object PerformanceUtils:
   val rangeValues = (lowerBound to upperBound)
 
   /* Linear sequences: List, ListBuffer */
-  val immList = rangeValues.toList
-  val mutList = ListBuffer.range(lowerBound, upperBound)
+  var immList = rangeValues.toList
+  var mutList = ListBuffer.range(lowerBound, upperBound)
   val randomUpdateIndex = Random().nextInt(position, upperBound)
+  println("[Linear Sequences]")
   assert(measure("[Immutable List] Get element")(immList(position)) > measure("[Mutable List] Get element")(mutList(position)))
   separate
+  println("Random position index: " + randomUpdateIndex)
   assert(measure("[Immutable List] Update element")(immList.updated(randomUpdateIndex, 2)) > measure("[Mutable List] Update Element")(mutList.update(randomUpdateIndex, 1))) 
   separate
-
-
-
+  assert(measure("[Immutable List] Remove element")(immList.drop(randomUpdateIndex)) < measure("[Mutable List] Remove element")(mutList.drop(randomUpdateIndex))) 
+  immList = null
+  mutList = null
   /* Indexed sequences: Vector, Array, ArrayBuffer */
-
+  separate
+  println("[Indexed Sequences]")
+  val vector = Vector.range(lowerBound, upperBound)
+  val array = Array.range(lowerBound, upperBound)
+  val arrayBuffer = ArrayBuffer.range(lowerBound, upperBound)
+  assert(measure("[Vector] Get element")(vector(position)) >= measure("[Array] Get element")(array(randomUpdateIndex)))
+  assert(measure("[Vector] Get element")(vector(position)) < measure("[ArrayBuffer] Get element")(arrayBuffer(randomUpdateIndex)))
+  separate
 
   /* Sets */
 
