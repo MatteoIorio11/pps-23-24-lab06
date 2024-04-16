26b745288cf937abb830267dcbadc23556c4418b
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-16 11:47:17 +0200

    feat: general refactor, create the method measureAll

--- src/main/scala/ex3/Collections.scala
+++ src/main/scala/ex3/Collections.scala
@@ -8,20 +8,23 @@ import java.util.Random
 import scala.collection.mutable.ArrayBuffer
 
 object PerformanceUtils:
-  case class MeasurementResults[T](result: T, duration: FiniteDuration) extends Ordered[MeasurementResults[_]]:
+  case class MeasurementResults[T](objectName: String, result: T, duration: FiniteDuration) extends Ordered[MeasurementResults[_]]:
     override def compare(that: MeasurementResults[_]): Int = duration.toNanos.compareTo(that.duration.toNanos)
+    override def toString(): String = "Result from the object: " + objectName + " is : " + duration
 
-  def measure[T](msg: String)(expr: => T): MeasurementResults[T] =
+  def measure[T](msg: String, className: String)(expr: => T): MeasurementResults[T] =
     val startTime = System.nanoTime()
     val res = expr
     val duration = FiniteDuration(System.nanoTime() - startTime, TimeUnit.NANOSECONDS)
     if (msg.nonEmpty) println(msg + " -- " + duration.toNanos + " nanos; " + duration.toMillis + "ms")
-    MeasurementResults(res, duration)
+    MeasurementResults(className, res, duration)
 
-  def measure[T](expr: => T): MeasurementResults[T] = measure("")(expr)
+  def measure[T](expr: => T): MeasurementResults[T] = measure("", "")(expr)
 
   def separate: Unit = println("<><><><><><><><><>")
 
+  def measureAll[A, T](operation: String)(expr: scala.collection.Seq[A]=> T)(lists: List[scala.collection.Seq[A]]): List[MeasurementResults[T]] = 
+    lists.map(list => measure(operation, list.getClass().toString())(expr(list))).toList
 @main def checkPerformance: Unit =
   import PerformanceUtils.*
   
@@ -35,29 +38,52 @@ object PerformanceUtils:
   var mutList = ListBuffer.range(lowerBound, upperBound)
   val randomUpdateIndex = Random().nextInt(position, upperBound)
   println("[Linear Sequences]")
-  assert(measure("[Immutable List] Get element")(immList(position)) > measure("[Mutable List] Get element")(mutList(position)))
+  measureAll("Get a specific element")(list => list(position))(List(immList, mutList))
+    .sorted
+    .foreach(m => println(m))
+  separate
+  measureAll("Update element")(list => list.updated(position, 2))(List(immList, mutList))
+    .sorted
+    .foreach(m => println(m)) 
+  separate
+  measureAll("Drop an element")(list => list.drop(randomUpdateIndex))(List(immList, mutList))
+    .sorted
+    .foreach(m => println(m))
+
+  //assert(measure("[Immutable List] Get element")(immList(position)) > measure("[Mutable List] Get element")(mutList(position)))
   separate
   println("Random position index: " + randomUpdateIndex)
-  assert(measure("[Immutable List] Update element")(immList.updated(randomUpdateIndex, 2)) > measure("[Mutable List] Update Element")(mutList.update(randomUpdateIndex, 1))) 
+  //assert(measure("[Immutable List] Update element")(immList.updated(randomUpdateIndex, 2)) > measure("[Mutable List] Update Element")(mutList.update(randomUpdateIndex, 1))) 
   separate
-  assert(measure("[Immutable List] Remove element")(immList.drop(randomUpdateIndex)) < measure("[Mutable List] Remove element")(mutList.drop(randomUpdateIndex))) 
+  //assert(measure("[Immutable List] Remove element")(immList.drop(randomUpdateIndex)) < measure("[Mutable List] Remove element")(mutList.drop(randomUpdateIndex))) 
   immList = null
   mutList = null
   /* Indexed sequences: Vector, Array, ArrayBuffer */
   separate
   println("[Indexed Sequences]")
-  val vector = Vector.range(lowerBound, upperBound)
-  val array = Array.range(lowerBound, upperBound)
-  val arrayBuffer = ArrayBuffer.range(lowerBound, upperBound)
-  assert(measure("[Vector] Get element")(vector(position)) >= measure("[Array] Get element")(array(randomUpdateIndex)))
-  assert(measure("[Vector] Get element")(vector(position)) < measure("[ArrayBuffer] Get element")(arrayBuffer(randomUpdateIndex)))
+  var vector = Vector.range(lowerBound, upperBound)
+  var array = Array.range(lowerBound, upperBound)
+  var arrayBuffer = ArrayBuffer.range(lowerBound, upperBound)
   separate
-
+  measureAll("[Indexed sequences]: Get element at position")(list => list(position))(List[scala.collection.Seq[Int]](vector, array, arrayBuffer))
+    .sorted
+    .foreach(m => println(m))
+  separate 
+  measureAll("[Indexed sequences]: Update element at position")(list => list.updated(randomUpdateIndex, 2))(List(vector, array, arrayBuffer))
+    .sorted
+    .foreach(m => println(m))
+  separate
+  measureAll("[Indexed sequences]: Drop an element")(list => list.drop(randomUpdateIndex))(List(vector, array, arrayBuffer))
+    .sorted
+    .foreach(m => println(m))
+  vector = null
+  array = null
+  arrayBuffer = null
   /* Sets */
-
+  println("[Sets]")
   /* Maps */
 
   /* Comparison */
   val lst = (1 to 10000000).toList
   val vec = (1 to 10000000).toVector
-  assert(measure("lst last")(lst.last) > measure("vec last")(vec.last))
+  //assert(measure("lst last")(lst.last) > measure("vec last")(vec.last))
